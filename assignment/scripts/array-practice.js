6e231f30d9f6d82a3af92880eaaa862256df5e24
Davey-M <davey.meuer@gmail.com>
2021-12-13 11:05:17 -0600

    Changed wording in assignement 3 part 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -51,7 +51,7 @@ console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
 favoriteFoods.push('Chocolate');
-console.log('Favotite foods:', favoriteFoods);
+console.log('Added Food to the end,', favoriteFoods);
 
 
 // Example: Remove the last animal by using Array.pop
